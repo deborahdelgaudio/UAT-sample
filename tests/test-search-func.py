7859f901c4a48411bc4a222bfcbd0579e6d408a3
DeborahDelGaudio <delgaudio.d@gmail.com>
2018-11-17 11:45:53 +0100

    added the firts check on test-search-func

--- tests/test-search-func.py
+++ tests/test-search-func.py
@@ -1,15 +1,25 @@
-# -*- coding: utf-8 -*-
-
+# -*- coding: <UTF-8> -*-
 from all_imports import *
 from user import *
 
 class Test_search_functionality(unittest.TestCase):
 
+    '''Start web driver'''
     def setUp(self):
         path = os.getcwd() + '/chrome-driver/chromedriver'
         self.driver = webdriver.Chrome(executable_path=path)
         self.url = 'https://www.autohero.com/de/search/'
 
+        ##        chrome_options = webdriver.ChromeOptions()
+        #chrome_options.add_argument('--no-sandbox')
+        #chrome_options.add_argument('--headless')
+        #chrome_options.add_argument('--disable-gpu')
+
+    '''Stop web driver'''
+    def tearDown(self):
+        self.driver.quit()
+
+    '''Filter for 2015 as minimum registration year and sort by descending price'''
     def test_if_items_are_correctly_filtered_and_sorted(self):
         driver = self.driver
         user = User(driver)
@@ -18,25 +28,44 @@ class Test_search_functionality(unittest.TestCase):
 
         driver.get(self.url)
 
-        #click web element
-        data_qa_selector = 'filter-year'
-        user.click_a_web_element(data_qa_selector)
+        user.click_a_web_element('filter-year')
 
-        #choose 2015 on year select
-        user.choose_a_value_in_a_select('yearRange.min', '2015')
+        user.choose_a_value_in_a_select('yearRange.min', '2015')  # choose 2015 on year select
 
-        #sort by desc price using a select
-        user.choose_a_value_in_a_select('sort', 'offerPrice.amountMinorUnits.desc')
+        user.choose_a_value_in_a_select('sort', 'offerPrice.amountMinorUnits.desc')  # sort by desc price using a select
+        print driver.current_url
+
+        current_url = driver.current_url
+
+        '''Verify that items has been filtered by checking: query string on url, applied filter and registration date on cards'''
+        self.assertIn('yearMin=2015', current_url, 'Query string is incorrect %s' %current_url)
+        time.sleep(2)
+        try:
+            filter_applied = driver.find_element_by_xpath('//*[@data-qa-selector="active-filter"]')
+            value_filter_applied = filter_applied.get_attribute('data-qa-selector-value')
+            value_filter_applied = int(value_filter_applied)
+            self.assertEqual(value_filter_applied, 2015)
+        except:
+            self.fail('It\'s not possible to see the active filter \'Erstzulassung von\'')
+
+        spec_list = driver.find_elements_by_xpath('//*[@data-qa-selector="spec-list"]')
+
+        reg_dates = []
+        for spec in spec_list:
+            date = spec.find_elements_by_xpath('//*[@data-qa-selector="spec"]')[0]
+            reg_dates.append(date)
+
+        reg_years = []
+        for date in reg_dates:
+            date_str = str(date.text.encode('ascii', 'replace')).split('/')[1]
+            year = int(date_str)
+            reg_years.append(year)
+
+        self.assertGreaterEqual(reg_years, 2015)
 
 
-
-
-        ##verify that items are filtered by year
         ##verify that items are sorted by desc price
         pass
-
-    def tearDown(self):
-        self.driver.quit()
 
 if __name__ == "__main__":
     unittest.main()
---
+++ tests/user.py
@@ -0,0 +1,37 @@
+from all_imports import *
+
+'''
+    User implements user's actions on browser
+'''
+
+class User ():
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def check_url_status(self, url):
+        res = requests.get(url)
+        if res.status_code == 200:
+            pass
+        else:
+            self.fail('Bad status code: %s' % res.status_code)
+
+    def click_a_web_element(self, data_qa_selector):
+        self.driver.find_element_by_xpath('//*[@data-qa-selector="%s"]' % data_qa_selector).click()
+
+    def choose_a_value_in_a_select(self, select_name, data_qa_selector):
+        select = Select(self.driver.find_element_by_name('%s' % select_name))
+        options = select.options
+
+        for option in options:
+            if option.get_attribute('data-qa-selector-value') == data_qa_selector:
+                corresponding_value = option.get_attribute('value')
+                element_selected = option
+                break
+            else:
+                pass
+
+        select.select_by_value('%s' % corresponding_value)
+
+        wait = WebDriverWait(self.driver, 8)
+        wait.until(EC.element_to_be_selected(element_selected))
