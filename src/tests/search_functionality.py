82cdfb7f1f3c27429ea5cddc51c202d5749d77fb
DeborahDelGaudio <delgaudio.d@gmail.com>
2019-03-25 22:15:55 +0100

    testfile adapted

--- src/tests/search_functionality.py
+++ src/tests/search_functionality.py
@@ -4,6 +4,7 @@ import unittest
 import time
 import yaml
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import requests
@@ -20,14 +21,15 @@ class SearchFunctionalityScenario(unittest.TestCase):
         self.url = self.data["url"]
 
         self.d.get(self.url)
-        self.d.find_element_by_class_name("root___1ZGR8").click()
+        time.sleep(1.5)
+        self.d.find_element(By.XPATH, "//div[@data-qa-selector='{}']".format(self.data["data_qa_selector"][0])).click()
         time.sleep(1.5)
 
         year_select = Select(self.d.find_element_by_name('%s' % self.data["selects"][0]))
-        year_select.select_by_visible_text("2016")
+        year_select.select_by_value("4")
 
         sort_select = Select(self.d.find_element_by_name('%s'%self.data["selects"][1]))
-        sort_select.select_by_visible_text("Höchster Preis")
+        sort_select.select_by_value("2")
 
     '''Stop web driver'''
     def tearDown(self):
@@ -36,20 +38,20 @@ class SearchFunctionalityScenario(unittest.TestCase):
     def test_if_items_are_correctly_filtered_and_sorted(self):
         """Test if items are correctly filtered and sorted"""
 
-        current_url = d.current_url
+        current_url = self.d.current_url
 
         '''Verify that items has been filtered by checking: query string on url, applied filter and registration date on cards'''
         self.assertIn('yearMin=2016', current_url, 'Query string is incorrect {}'.format(current_url))
         time.sleep(2)
         try:
-            filter_applied = d.find_element_by_xpath('//*[@data-qa-selector="{}"]'.format(self.data["data_qa_selector"][3]))
+            filter_applied = self.d.find_element_by_xpath('//*[@data-qa-selector="{}"]'.format(self.data["data_qa_selector"][3]))
             value_filter_applied = filter_applied.get_attribute('data-qa-selector-value')
             value_filter_applied = int(value_filter_applied)
             self.assertEqual(value_filter_applied, 2016)
         except:
             self.fail('It\'s not possible to see the active filter \'Registration year\'')
 
-        spec_list = d.find_elements_by_xpath('//*[@data-qa-selector=""]'.format(self.data["data_qa_selector"][4]))
+        spec_list = self.d.find_elements_by_xpath('//*[@data-qa-selector=""]'.format(self.data["data_qa_selector"][4]))
 
         reg_dates = []
         for spec in spec_list:
@@ -62,13 +64,13 @@ class SearchFunctionalityScenario(unittest.TestCase):
             year = int(date_str)
             reg_years.append(year)
 
-        self.assertGreaterEqual(reg_years, 2016)
+        self.assertTrue((all(year >= 2016 for year in reg_years)), "Registration years are not correctly selected")
 
 
         '''Verify that items are sorted by descending price by checking: query string and price on cards'''
         self.assertIn('sort=PRICE_DESC', current_url, 'Query string is incorrect {}'.format(current_url))
 
-        prices = d.find_elements_by_xpath('//*[@data-qa-selector=""]'.format(self.data["data_qa_selector"][6]))
+        prices = self.d.find_elements_by_xpath('//*[@data-qa-selector=""]'.format(self.data["data_qa_selector"][6]))
 
         price_list = []
         for price in prices:
